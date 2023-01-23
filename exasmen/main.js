536793b0109ec6940a02570893ece434f28e66cc
kevinpaul99 <90939217+kevinpaul99@users.noreply.github.com>
2023-01-23 10:52:03 -0500

    Add files via upload

---
+++ exasmen/main.js
@@ -0,0 +1,61 @@
+function hora(){
+    var a = document.getElementById("q").value;
+    if(a<=3 && a>=1 ){
+        document.getElementById("re").innerHTML="Futbol";
+    }
+    if(a>3 && a<=6){
+        document.getElementById("re").innerHTML="Futbol Americano"
+    }
+    if(a>6 && a<=9){
+        document.getElementById("re").innerHTML="Atletismo"
+    }
+}
+function vh(){
+    let s=document.getElementById('s');
+    let sl=s.value;
+    document.getElementById('rec').innerText = `${sl}.`;
+}
+function veri(){
+    var re=document.getElementById("num").value
+    if(re<=-1){
+        document.getElementById("w").innerHTML="El numero " + re + " es negativo";
+    }else{
+        document.getElementById("w").innerHTML="El numero "+ re + " es positivo";
+    }
+}
+function verifi(){
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+    var n3 = document.getElementById("n3").value;
+    if(n1>n2 && n1>n3){
+        document.getElementById("e").innerHTML="El numero mayor es: " + n1;
+    }
+    if(n2>n1 && n2>n3){
+        document.getElementById("e").innerHTML="El numero mayor es: " + n2;
+    }
+    if(n3>n1 && n3>n2){
+        document.getElementById("e").innerHTML="El numero mayor es: " + n3;
+    }
+}
+function hola(){
+    let f=document.getElementById('operacion');
+    var fl=(f.value);
+    document.getElementById("e").innerHTML=fl;
+    var res= (n1) (fl) (n2);
+    document.getElementById("a").innerHTML=res;
+}
+function edad(){
+    var ed=parseInt(document.getElementById("edad").value);
+    if(n1>=18){
+        document.getElementById("jh").innerHTML="Eres mayor de edad";
+    }else{
+        document.getElementById("jh").innerHTML="Eres menor de edad";
+    }
+}
+function Resul(){
+    var a =parseInt(document.getElementById("no1").value);
+    var b =parseInt(document.getElementById("no2").value);
+    var c =parseInt(document.getElementById("no3").value);
+    var d =parseInt(document.getElementById("no4").value);
+    var e =parseInt(document.getElementById("no5").value);
+}
